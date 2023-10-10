c36aaad648b9f8a127555b2418c2810ca65a172f
Emilio Jesus Gallego Arias <e+git@x80.org>
2023-10-10 19:57:06 +0200

    [tmp] Disable math-comp until we fix sdk bug

--- lesson_2.v
+++ lesson_2.v
@@ -1,5 +1,6 @@
 (** * Lesson 2: Data Structures and Class Hierarchies *)
-From mathcomp Require Import all_ssreflect.
+From Coq Require Import ssreflect.
+(* From mathcomp Require Import all_ssreflect. *)
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -28,23 +29,23 @@ Coq Standard library vs as it is defined in math-comp: *)
 About le.
 Print le.
 
-About leq.
-Print leq.
+(* About leq. *)
+(* Print leq. *)
 
 Lemma le_1 : 100 <= 200.
 Proof. Admitted.
 
 Print le_1.
 
-Lemma le_2 : (100 <= 200)%coq_nat.
-Proof. Admitted.
+(* Lemma le_2 : (100 <= 200)%coq_nat. *)
+(* Proof. Admitted. *)
 
-Print le_2.
+(* Print le_2. *)
 
-Lemma lt_1 n : ~~ (n < n).
-Proof. Admitted.
+(* Lemma lt_1 n : ~~ (n < n). *)
+(* Proof. Admitted. *)
 
-Lemma lt_2 n : ~ (n < n)%coq_nat.
-Proof. Admitted.
+(* Lemma lt_2 n : ~ (n < n)%coq_nat. *)
+(* Proof. Admitted. *)
 (** Moreover, the math-comp version, by virtue of being in [bool],
 inherits some nice properties, like decidability, for free. *)
